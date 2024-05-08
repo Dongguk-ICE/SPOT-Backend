001b632c49f9130f8985010d82fa601850b13307
JeongHeumChoi <79458446+JeongHeumChoi@users.noreply.github.com>
2024-05-08 21:28:58 +0900

    ✨ [Feature] - Reissue 토큰 기능 추가 (#31)
    * Chore: Credentials 수정
    
    * Feat: Reissue 토큰 기능 구현
    
    * Chore: Credentials 추가

--- src/main/java/ice/spot/service/AuthService.java
+++ src/main/java/ice/spot/service/AuthService.java
@@ -2,9 +2,11 @@ package ice.spot.service;
 
 import ice.spot.domain.User;
 import ice.spot.dto.request.OauthSignUpDto;
+import ice.spot.dto.response.JwtTokenDto;
 import ice.spot.exception.CommonException;
 import ice.spot.exception.ErrorCode;
 import ice.spot.repository.UserRepository;
+import ice.spot.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
 
     @Transactional
     public void signUp(Long userId, OauthSignUpDto oauthSignUpDto){
@@ -25,4 +28,15 @@ public class AuthService {
         oauthUser.register(oauthSignUpDto.nickname());
     }
 
+    @Transactional
+    public JwtTokenDto reGenerateTokens(Long userId, String refreshToken){
+        log.info("re generate tokens 진입성공");
+        User loginUser = userRepository.findByIdAndRefreshToken(userId, refreshToken)
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
+        log.info("유저 조회 성공");
+        JwtTokenDto jwtTokenDto = jwtUtil.generateTokens(loginUser.getId(), loginUser.getRole());
+
+        loginUser.updateRefreshToken(jwtTokenDto.refreshToken());
+        return jwtTokenDto;
+    }
 }
