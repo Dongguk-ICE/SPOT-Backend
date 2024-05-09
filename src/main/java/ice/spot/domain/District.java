1c2cd336c8621cfb391fa16a1531a84ad0d6c988
Jang99u <alswkdrb@dgu.ac.kr>
2024-05-09 17:18:56 +0900

    Feat: District 도메인 생성

---
+++ src/main/java/ice/spot/domain/District.java
@@ -0,0 +1,34 @@
+package ice.spot.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "district")
+public class District {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "detail_address")
+    private String detailAddress;
+
+    @Column(name = "grid_x")
+    private Integer gridX;
+
+    @Column(name = "grid_y")
+    private Integer gridY;
+
+    @Column(name = "lat")
+    private Double lat;
+
+    @Column(name = "lon")
+    private Double lon;
+}
