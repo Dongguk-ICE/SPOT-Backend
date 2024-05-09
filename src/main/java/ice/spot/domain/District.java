a8b6229503a6c8ec7ac0ecdcf01da386a0fbf658
Jang99u <alswkdrb@dgu.ac.kr>
2024-05-09 22:57:36 +0900

    Feat: District 도메인 컬럼 추가

--- src/main/java/ice/spot/domain/District.java
+++ src/main/java/ice/spot/domain/District.java
@@ -14,6 +14,9 @@ public class District {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "territory")
+    private String territory;
+
     @Column(name = "address")
     private String address;
 
